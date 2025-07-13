2a0c68195f6fa7fc569c9cc4818ba39df752c4a8
DoliScriptz <132125366+DoliScriptz@users.noreply.github.com>
2025-07-13 15:38:27 +0700

    Update notify.js

--- api/notify.js
+++ api/notify.js
@@ -11,37 +11,45 @@ export default async function handler(req, res) {
   const check = crypto.createHmac('sha256', process.env.HWID_SECRET).update(payload).digest('hex');
   if (sig !== check) return res.status(403).end();
   if (Date.now() > parseInt(exp)) return res.status(403).end();
+
   const mainWebhook = process.env.WEBHOOK_URL;
   const secretWebhook = process.env.SECRET_WEBHOOK_URL;
   if (!mainWebhook || !secretWebhook) return res.status(500).end();
+
+  const desc = [];
   for (const item of found) {
-    const embed = {
-      title: `${item.rarity} Found - ${item.displayName}`,
-      description: `[Join Here](${link})`,
-      color: 0x00FF00,
-      fields: [
-        { name: "Owner", value: item.owner, inline: true },
-        { name: "Rarity", value: item.rarity, inline: true },
-        { name: "Players Inside", value: `${players}`, inline: true }
-      ],
-      timestamp: new Date().toISOString(),
-      footer: {
-        text: "Makal Hub • Brainrot Notifier",
-        icon_url: "https://media.discordapp.net/attachments/1279117246836375593/1384781511697633310/download.webp"
-      }
-    };
-    await fetch(mainWebhook, {
+    desc.push(`- Owner: ${item.owner}`);
+    desc.push(`• ${item.displayName} - ${item.rarity}`);
+  }
+
+  const embed = {
+    title: `Brainrot(s) Found`,
+    description: desc.join('\n'),
+    color: 0x00FF00,
+    fields: [
+      { name: "Players Inside", value: `${players}`, inline: true },
+      { name: "Join Link", value: `[Join Here](${link})`, inline: false }
+    ],
+    timestamp: new Date().toISOString(),
+    footer: {
+      text: "Makal Hub • Brainrot Notifier",
+      icon_url: "https://media.discordapp.net/attachments/1279117246836375593/1384781511697633310/download.webp"
+    }
+  };
+
+  await fetch(mainWebhook, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ embeds: [embed] })
+  });
+
+  if (found.some(x => x.rarity === "Secret")) {
+    await fetch(secretWebhook, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ embeds: [embed] })
     });
-    if (item.rarity === "Secret") {
-      await fetch(secretWebhook, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ embeds: [embed] })
-      });
-    }
   }
+
   return res.status(200).end();
 }
